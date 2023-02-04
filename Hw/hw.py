cefb953936421839344eb7524265ea03bae0c4bd
Paolo D'Alberto <paolo.dalberto@gmail.com>
2023-02-04 11:59:27 -0800

    introducing real instruction for accelerators

--- Hw/hw.py
+++ Hw/hw.py
@@ -2,6 +2,31 @@ from Matrices.matrices import Matrix
 from multiprocessing import Process, Queue, Pool, Manager
 from Graph.graph import Graph
 
+
+
+
+
+class GPU:
+    def __init__():
+
+
+        self.GEMM = """rocblas_status rocblas_dgemm(rocblas_handle handle, rocblas_operation transA, rocblas_operation transB, rocblas_int m, rocblas_int n, rocblas_int k, const double *alpha, const double *A, rocblas_int lda, const double *B, rocblas_int ldb, const double *beta, double *C, rocblas_int ldc) """
+
+        ## handle, M, N, K, alpha, A,  LDA=K, B, LDB=N, beta,C, LDC=N 
+        self.GEMM_x = "rocblas_dgemm( %s, 'n', 'n', %d, %d, %d,  %s, %s, %d, %s, %d,  %s, %s, %d) "
+
+        self.GEMA = """rocblas_status rocblas_dgema(rocblas_handle handle, rocblas_operation transA, rocblas_operation transB, rocblas_int m, rocblas_int n, rocblas_int k, const double *alpha, const double *A, rocblas_int lda, const double *B, rocblas_int ldb, const double *beta, double *C, rocblas_int ldc) """
+
+        ## handle, M, N, alpha, A, beta, LDA=K, B, LDB=N, beta,C, LDC=N 
+        self.GEMA_x = "rocblas_dgema( %s, 'n', 'n', %d, %d,  %s, %s, %d,  %s, %s, %d,  %s, %d) "
+
+
+
+
+
+
+
+
 class Memory:
     def __init__(self,
                  name :str, 
