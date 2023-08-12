41d8f0f5e609a87c3bb5748d0e205e3ea18c9015
Paolo D'Alberto <paolo.dalberto@gmail.com>
2023-08-12 16:04:01 -0700

    please

--- docker_pip.sh
+++ docker_pip.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=$PWD:$PWD/GpuInterface/build/lib.linux-x86_64-cpython-39/:$PWD/GpuInterface
-pip install networkx matplotlib graphviz seaborn p11bind
+pip install networkx matplotlib graphviz seaborn pybind11
