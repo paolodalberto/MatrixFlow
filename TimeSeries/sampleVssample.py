b4a1751e5dea733cbff28eb1a794568fb72a84db
Paolo D'Alberto <paolo.dalberto@gmail.com>
2025-02-02 11:00:24 -0800

    place holder for distributions and such

---
+++ TimeSeries/sampleVssample.py
@@ -0,0 +1,17 @@
+###
+## You have a set of experiments in the past, say N samples 
+## You have a set today, say M sample.
+##
+## Can we say any thing if M belongs to N?  do M have the same
+## properties of N or not ?
+##
+###
+
+
+import numpy
+import math
+import scipy 
+
+
+
+
